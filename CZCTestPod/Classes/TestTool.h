9844bc8ab254258abe3f40eb906ffc80eb281f58
chenzhichao <chenzc@gegejia.com>
2020-05-13 18:13:16 +0800

    初始化

---
+++ CZCTestPod/Classes/TestTool.h
@@ -0,0 +1,17 @@
+//
+//  TestTool.h
+//  Slider
+//
+//  Created by chenzhichao on 2020/5/13.
+//  Copyright © 2020 chenzhichao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestTool : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CZCTestPod/Classes/TestTool.m
@@ -0,0 +1,13 @@
+//
+//  TestTool.m
+//  Slider
+//
+//  Created by chenzhichao on 2020/5/13.
+//  Copyright © 2020 chenzhichao. All rights reserved.
+//
+
+#import "TestTool.h"
+
+@implementation TestTool
+
+@end
